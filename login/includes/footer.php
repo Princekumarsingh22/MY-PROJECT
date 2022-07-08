0e7ba8277d279151fae1e100e80400d045c3e3b1
Prince@1213 <linuxhjp1213@gmail.com>
2022-07-08 14:15:45 +0530

    change

--- login/includes/footer.php
+++ login/includes/footer.php
@@ -1,5 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
