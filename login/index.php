d56a92d5a953cd1ba539ba16d2cc4c2df125c47d
Prince@1213 <linuxhjp1213@gmail.com>
2022-07-08 13:42:30 +0530

    new

---
+++ login/index.php
@@ -0,0 +1,14 @@
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar.php'); ?>
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>Login and Registration system in PHP</h4>
+                <h5>with email verification</h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
