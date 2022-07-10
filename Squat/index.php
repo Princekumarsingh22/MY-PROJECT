b3fd4489cffb9f22d043a77b83fc6eed9a124879
Prince@1213 <linuxhjp1213@gmail.com>
2022-07-10 11:53:46 +0530

    update

--- Squat/index.php
+++ Squat/index.php
@@ -1,6 +1,5 @@
 <?php
     include('includes/header.php');
     include('includes/navbar1.php');
-    include('wrong.php');
     include('includes/footer1.php');
     ?>
---
+++ Squat/wrong.php
@@ -0,0 +1,65 @@
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar2.php'); ?>
+<style>
+    .disclaimer-container {
+  display: flex;
+  justify-content: center;
+  height: auto;
+  margin-bottom: 50px;
+}
+
+.disclaimer-container > div {
+  border: 1px solid #1e1b22;
+  box-shadow: 10px 10px 1px #082c2c8a;
+}
+
+.cardd ul li {
+  font-family: "PT Sans", sans-serif;
+  color: #6ab992;
+  line-height: 40px;
+  font-size: 20px;
+}
+
+.cardd a {
+  text-decoration: none;
+  font-weight: bold;
+  color: #6ab992;
+  font-style: italic;
+}
+
+.cardd a:hover {
+  color: #082c2c;
+}
+
+</style>
+<!-- Main content section -->
+<main class="row">
+        <div class="col-12">
+          <h1 class="sub-head">Desclaimer..!</h1>
+          <div class="disclaimer-container">
+            <div class="card cardd col-11 col-md-9 col-lg-8 col-xl-4">
+              <ul>
+                <li>This is the demo project website.</li>
+                <li>
+                  The photos and all other contents used in website<br />taken
+                  from Google, only for demo.
+                </li>
+                <li>
+                  The company name used is taken by self not under any company
+                  or property.
+                </li>
+                <li>
+                  The design is made by the respective owner of the website
+                </li>
+                <li>
+                  If you have any queries, to contact the website owner
+                  <a href="https://linktr.ee/darshanr" target="_blank"
+                    >click here</a
+                  >.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+<?php include('includes/footer2.php'); ?>
